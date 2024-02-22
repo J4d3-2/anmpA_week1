fa17abbe33d8b098202fd6fa321dbfd32203f3be
ASUS <ivana.liman12@gmail.com>
2024-02-22 14:29:13 +0700

    sign up activity comment

--- app/src/main/java/com/jubaya/anmpweek1/SignupActivity.kt
+++ app/src/main/java/com/jubaya/anmpweek1/SignupActivity.kt
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SignupActivity : AppCompatActivity() {
+
+    //SIGN UP Commented
+    //cek 1
+    //cek 2
+    //cek 3
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
